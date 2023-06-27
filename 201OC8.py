fd7b474c02d88f809574d257414d9814ca35c50e
PaulineFoust <misspaulinefoust@gmail.com>
2023-06-26 21:55:47 -0400

    first commit

---
+++ 201OC8.py
@@ -0,0 +1,26 @@
+# Script: Ops 301 Class 08 Ops Challenge Solution
+# Author: Pauline Foust
+# Ops Challenge: Python Collections
+# Date of latest revision: 6/26/23
+# Purpose: Python Exercise# Objectives
+
+# Create a Python script that includes the following:
+# Assign to a variable a list of ten string elements.
+# Print the fourth element of the list.
+# Print the sixth through tenth element of the list.
+# Change the value of the seventh element to “onion”.
+
+# Assign to a variable a list of ten string elements.
+myList = ['apple', 'banana', 'grapes', 'cherry', 'kiwi', 'lemon','watermelon', 'apricots', 'pineapple', 'orange']
+
+# Print the fourth element of the list.
+fourthElement = myList[3]
+print(fourthElement)
+
+# Print the sixth through tenth element of the list.
+sixElement = myList[5]
+print(sixElement)
+
+# Change the value of the seventh element to “onion”.
+myList[6] = 'onion'
+print(myList)
